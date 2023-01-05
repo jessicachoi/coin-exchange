ee71db85b168c0278002c79c33965be73940fcfa
Jessica Choi <jessicachoi@Jessicas-MacBook-Pro.local>
2023-01-05 21:54:47 +0900

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div>
+        Card
+      </div>
+    )
+  }
+}
